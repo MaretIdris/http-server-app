5702c55871a043c813da130bed3293301d5f8f8e
Maret Idris <maret@fasterlap.com>
2022-01-25 12:55:21 -0800

    Add discriminated union types

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,5 +1,51 @@
-import { helloWorld } from "../main"
+const url = new URL("https://neilmadden.blog:8080/2021/10/27/multiple-input-macs?key1=value1&key2=value2#:~:text=Other%20approaches%20to%20multi%2Dinput%20MACs")
 
-test("helloWorld returns a string", () => {
-  expect(helloWorld()).toEqual("hello world")
+test("url hostname works", () => {
+  const string = "http://localhost:8080/api"
+  const url = new URL(string)
+  expect(url.hostname).toEqual("localhost")
+})
+
+test("url hash works", () => {
+  expect(url.hash).toEqual("#:~:text=Other%20approaches%20to%20multi%2Dinput%20MACs")
+})
+
+test("url hostname works", () => {
+  expect(url.hostname).toEqual("neilmadden.blog")
+})
+
+test("url port works", () => {
+  expect(url.port).toEqual("8080")
+})
+
+test("url pathname works", () => {
+  expect(url.pathname).toEqual("/2021/10/27/multiple-input-macs")
+})
+
+test("url path works", () => {
+  // path is combination of pathname and search query
+  const pathname = url.pathname
+  const searchQuery = url.search
+  const path = pathname + searchQuery
+  expect(path).toEqual("/2021/10/27/multiple-input-macs?key1=value1&key2=value2")
+})
+
+test("url search works", () => {
+  expect(url.search).toEqual("?key1=value1&key2=value2")
+})
+
+test("url searchParams work", () => {
+  const keys = []
+  const values = []
+  for(const [key, value] of url.searchParams.entries()) {
+    keys.push(key)
+    values.push(value)
+  }
+  expect(keys).toEqual(["key1", "key2"])
+  expect(values).toEqual(["value1", "value2"])
+})
+
+test("url host works", () => {
+  // host is a combination of the hostname and port
+  expect(url.host).toEqual("neilmadden.blog:8080")
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,61 @@
-const main = () => {
-  console.log(helloWorld())
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+const PORT = 8082;
+
+type Currency = "USD" | "EUR" | "GBP"
+
+type BaseUSD = {
+  USD: Partial<Record<Currency, number>>
+}
+type BaseEUR = {
+  EUR : Partial<Record<Currency, number>>
+}
+type BaseGBP = {
+  GBP: Partial<Record<Currency, number>>
 }
 
-export const helloWorld = () => {
-  return "hello world"
+const mockCurrencyDataUSD: BaseUSD = {
+  USD: {
+    EUR: 1.21,
+    GBP: 1.11,
+  },
 }
 
-main()
+const mockCurrencyDataEUR: BaseEUR = {
+  EUR: {
+    USD: 1.21,
+    GBP: 1.11,
+  },
+}
+
+const mockCurrencyDataGBP: BaseGBP = {
+  GBP: {
+    USD: 1.21,
+    EUR: 1.11,
+  },
+}
+const updatedMockCurrencyDataUSD = {}
+
+let originalData = true
+
+function handleIncomingHTTPRequest(request: IncomingMessage, response: ServerResponse) {
+
+  // If request URL href is http://mymockapi.com/api/currencydata.json&base=USD return below
+  const url = request.url
+  if (originalData) {
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(mockCurrencyDataUSD))
+    response.end()
+  } else {
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(updatedMockCurrencyDataUSD))
+    response.end()
+  }
+
+  originalData = !originalData
+
+}
+
+const mockAPIServer = createServer(handleIncomingHTTPRequest);
+
+mockAPIServer.listen(PORT);
