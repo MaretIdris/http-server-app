47ebb84d6652655f06184edcbe59fc4158b209b1
Maret Idris <maret@fasterlap.com>
2022-01-26 14:34:30 -0800

    Update server app

--- src/main.ts
+++ src/main.ts
@@ -4,58 +4,77 @@ const PORT = 8082;
 
 type Currency = "USD" | "EUR" | "GBP"
 
-type BaseUSD = {
-  USD: Partial<Record<Currency, number>>
+interface BaseUSD {
+  USD: Partial<{[P in Currency]: number}>
 }
-type BaseEUR = {
-  EUR : Partial<Record<Currency, number>>
+
+interface BaseEUR {
+  EUR: Partial<Record<Currency, number>>
 }
-type BaseGBP = {
+
+interface BaseGBP {
   GBP: Partial<Record<Currency, number>>
 }
 
-const mockCurrencyDataUSD: BaseUSD = {
+const mockUSDData: BaseUSD = {
   USD: {
-    EUR: 1.21,
-    GBP: 1.11,
-  },
+    EUR: 0.88,
+    GBP: 0.74
+  }
 }
 
-const mockCurrencyDataEUR: BaseEUR = {
+const mockEURData: BaseEUR = {
   EUR: {
-    USD: 1.21,
-    GBP: 1.11,
-  },
+    USD: 1.13,
+    GBP: 0.84
+  }
 }
 
-const mockCurrencyDataGBP: BaseGBP = {
+const mockGBPData: BaseGBP = {
   GBP: {
-    USD: 1.21,
-    EUR: 1.11,
-  },
+    USD: 1.35,
+    EUR: 1.20
+  }
 }
-const updatedMockCurrencyDataUSD = {}
-
-let originalData = true
 
 function handleIncomingHTTPRequest(request: IncomingMessage, response: ServerResponse) {
+  // All the endpoints:
+  // http://localhost:8082/USD
+  // http://localhost:8082/EUR
+  // http://localhost:8082/GBP
 
-  // If request URL href is http://mymockapi.com/api/currencydata.json&base=USD return below
   const url = request.url
-  if (originalData) {
+
+  const respondWithCurrency = (mockData: BaseUSD | BaseEUR | BaseGBP) => {
     response.setHeader('Content-Type', 'application/json');
-    response.write(JSON.stringify(mockCurrencyDataUSD))
-    response.end()
-  } else {
-    response.setHeader('Content-Type', 'application/json');
-    response.write(JSON.stringify(updatedMockCurrencyDataUSD))
+    response.write(JSON.stringify(mockData))
     response.end()
   }
 
-  originalData = !originalData
+  switch (url) {
+    case "/USD": {
+      respondWithCurrency(mockUSDData)
+      break
+    }
+    case "/EUR": {
+      respondWithCurrency(mockEURData)
+      break
+    }
+    case "/GBP": {
+      respondWithCurrency(mockGBPData)
+      break
+    }
+    default: {
+      response.setHeader('Content-Type', 'text/html');
+      response.write("<html>")
+      response.write("<p>Your <span style='color: deeppink'>endpoint</span> is not matching any of the data we have available</p>")
+      response.write("</html>")
+      response.end()
+    }
+  }
 
 }
 
 const mockAPIServer = createServer(handleIncomingHTTPRequest);
 
-mockAPIServer.listen(PORT);
+mockAPIServer.listen(process.env.PORT || PORT);
